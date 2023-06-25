56ca0d4a9dd35a41ce1619c5ed42f801596ca132
ocasodi <david.l.markel@gmail.com>
2023-06-24 22:43:05 -0400

    serializer

--- backend/spanish_inquisition/views.py
+++ backend/spanish_inquisition/views.py
@@ -1,27 +1,15 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-import json
-
-User = get_user_model()
+from rest_framework.parsers import JSONParser
+from .serializers import UserSerializer
 
 @csrf_exempt
 def register(request):
     if request.method == 'POST':
-        # Load JSON data from the request
-        data = json.loads(request.body)
-
-        # Create a new user with the loaded data
-        # Make sure to hash the password
-        new_user = User.objects.create(
-            first_name=data['firstName'],
-            last_name=data['lastName'],
-            username=data['username'],
-            password=make_password(data['password']),
-            user_type=data['userType'],
-        )
-
-        return JsonResponse({'message': 'Registration successful'})
-
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
     return JsonResponse({'error': 'Invalid method'}, status=405)
