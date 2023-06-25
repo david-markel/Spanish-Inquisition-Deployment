3194cf25fd8ed6603cddef440f49922e4db5bb5a
ocasodi <david.l.markel@gmail.com>
2023-06-24 22:24:59 -0400

    REGISTER OMG

--- backend/spanish_inquisition/models.py
+++ backend/spanish_inquisition/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPES = (
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+
+    user_type = models.CharField(max_length=7, choices=USER_TYPES, default='student')
--- backend/spanish_inquisition/views.py
+++ backend/spanish_inquisition/views.py
@@ -1,8 +1,27 @@
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+import json
+
+User = get_user_model()
 
 @csrf_exempt
 def register(request):
-    # Handle the registration logic here
-    # You can simply return a success response for now
-    return JsonResponse({'message': 'Registration successful'})
+    if request.method == 'POST':
+        # Load JSON data from the request
+        data = json.loads(request.body)
+
+        # Create a new user with the loaded data
+        # Make sure to hash the password
+        new_user = User.objects.create(
+            first_name=data['firstName'],
+            last_name=data['lastName'],
+            username=data['username'],
+            password=make_password(data['password']),
+            user_type=data['userType'],
+        )
+
+        return JsonResponse({'message': 'Registration successful'})
+
+    return JsonResponse({'error': 'Invalid method'}, status=405)
