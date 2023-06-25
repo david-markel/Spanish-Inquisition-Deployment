f8c6b2e87d8c10f10b3d7add6e23a87ac39aa075
ocasodi <david.l.markel@gmail.com>
2023-06-24 22:50:23 -0400

    login

--- backend/spanish_inquisition/urls.py
+++ backend/spanish_inquisition/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import register, login
 
 urlpatterns = [
     path('auth/register', register, name='register'),
+    path('auth/login', login, name='login'),
 ]
--- backend/spanish_inquisition/views.py
+++ backend/spanish_inquisition/views.py
@@ -2,6 +2,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+import json
 
 @csrf_exempt
 def register(request):
@@ -12,4 +15,29 @@ def register(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+    return JsonResponse({'error': 'Invalid method'}, status=405)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # A backend authenticated the credentials
+            refresh = RefreshToken.for_user(user)
+            res = {
+                'token': str(refresh.access_token),
+                'firstName': user.first_name,
+                'lastName': user.last_name,
+                'username': user.username,
+                'userType': user.user_type,
+            }
+            return JsonResponse(res, status=200)
+        else:
+            # No backend authenticated the credentials
+            return JsonResponse({'error': 'Invalid credentials'}, status=401)
     return JsonResponse({'error': 'Invalid method'}, status=405)
